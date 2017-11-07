49ef7a137db1963f2236b0836beab491aab2ca19
baiyuting <baiyuting@vipkid.com.cn>
2017-11-07 16:50:07 +0800

    springboot配置文件讲解

---
+++ 第2课-springboot配置文件说明/src/main/java/com/example/bai02profile/Bai02profileApplication.java
@@ -0,0 +1,12 @@
+package com.example.bai02profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bai02profileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bai02profileApplication.class, args);
+	}
+}
