f8afbad489bd7170be127d311f538ec1ee6a2ce6
baiyuting <baiyuting@vipkid.com.cn>
2017-11-28 09:07:10 +0800

    第10课 提交

---
+++  第10课-用spring Restdocs创建API文档/src/test/java/com/example/bai10restdocs/restdocs/WebLayerTest.java
@@ -0,0 +1,37 @@
+package com.example.bai10restdocs.restdocs;
+
+import com.example.bai10restdocs.controller.HomeController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultHandler;
+
+import javax.annotation.Resource;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
+@AutoConfigureRestDocs(outputDir = "target/snippets")
+public class WebLayerTest {
+
+    @Resource
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello World")))
+                .andDo(document("home"));
+    }
+
+}
