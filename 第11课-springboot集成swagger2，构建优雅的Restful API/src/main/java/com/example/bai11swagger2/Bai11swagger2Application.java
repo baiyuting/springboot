6dab48e485532e5aceb2f75670778767b1394640
baiyuting <baiyuting@vipkid.com.cn>
2017-11-28 10:12:50 +0800

    第11课-springboot集成swagger2，构建优雅的Restful API

---
+++ 第11课-springboot集成swagger2，构建优雅的Restful API/src/main/java/com/example/bai11swagger2/Bai11swagger2Application.java
@@ -0,0 +1,12 @@
+package com.example.bai11swagger2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bai11swagger2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bai11swagger2Application.class, args);
+	}
+}
