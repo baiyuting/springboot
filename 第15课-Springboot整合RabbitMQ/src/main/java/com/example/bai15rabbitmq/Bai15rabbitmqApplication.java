6268c43d286509b7c6afd7ead52a9df06235b368
baiyuting <baiyuting@vipkid.com.cn>
2017-12-04 11:12:58 +0800

    第15课-Springboot整合RabbitMQ

---
+++ 第15课-Springboot整合RabbitMQ/src/main/java/com/example/bai15rabbitmq/Bai15rabbitmqApplication.java
@@ -0,0 +1,12 @@
+package com.example.bai15rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bai15rabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bai15rabbitmqApplication.class, args);
+	}
+}
