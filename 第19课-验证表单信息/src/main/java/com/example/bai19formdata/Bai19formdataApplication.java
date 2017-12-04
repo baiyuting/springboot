43a4aca799024468aeb2b25118d6df8192c98958
baiyuting <baiyuting@vipkid.com.cn>
2017-12-04 18:08:56 +0800

    第19课-验证表单信息

---
+++ 第19课-验证表单信息/src/main/java/com/example/bai19formdata/Bai19formdataApplication.java
@@ -0,0 +1,12 @@
+package com.example.bai19formdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bai19formdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bai19formdataApplication.class, args);
+	}
+}
---
+++ 第19课-验证表单信息/src/test/java/com/example/bai19formdata/Bai19formdataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.bai19formdata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Bai19formdataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
