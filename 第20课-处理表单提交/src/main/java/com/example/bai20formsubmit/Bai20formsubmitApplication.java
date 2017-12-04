e171d58648d860fab6aac01b387b2e0716edf6ea
baiyuting <baiyuting@vipkid.com.cn>
2017-12-04 19:59:09 +0800

    第20课-处理表单提交

---
+++ 第20课-处理表单提交/src/main/java/com/example/bai20formsubmit/Bai20formsubmitApplication.java
@@ -0,0 +1,12 @@
+package com.example.bai20formsubmit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bai20formsubmitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bai20formsubmitApplication.class, args);
+	}
+}
