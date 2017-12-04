4bc3b81e2a3fdafcd320d6f8ea0553de33a0b06e
baiyuting <baiyuting@vipkid.com.cn>
2017-12-04 14:54:27 +0800

    第16课-用restTemplate消费服务

---
+++ 第16课-用restTemplate消费服务/src/main/java/com/example/bai16resttemplate/Bai16resttemplateApplication.java
@@ -0,0 +1,12 @@
+package com.example.bai16resttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bai16resttemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bai16resttemplateApplication.class, args);
+	}
+}
