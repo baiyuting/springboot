6734d56251f97718f59202baca8a59db1238766d
baiyuting <baiyuting@vipkid.com.cn>
2017-12-04 15:33:53 +0800

    第17课-上传文件

---
+++ 第17课-上传文件/src/main/java/com/example/bai17uploadfile/Bai17uploadfileApplication.java
@@ -0,0 +1,12 @@
+package com.example.bai17uploadfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bai17uploadfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bai17uploadfileApplication.class, args);
+	}
+}
