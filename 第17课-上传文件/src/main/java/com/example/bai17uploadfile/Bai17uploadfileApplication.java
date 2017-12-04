52ce63cedf6fd5e8168958adc3ea5a4d8c6782a7
baiyuting <baiyuting@vipkid.com.cn>
2017-12-04 17:38:03 +0800

    第17课-上传文件 结束

--- 第17课-上传文件/src/main/java/com/example/bai17uploadfile/Bai17uploadfileApplication.java
+++ 第17课-上传文件/src/main/java/com/example/bai17uploadfile/Bai17uploadfileApplication.java
@@ -1,12 +1,26 @@
 package com.example.bai17uploadfile;
 
+import com.example.bai17uploadfile.storage.StorageProperties;
+import com.example.bai17uploadfile.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = {StorageProperties.class})
 public class Bai17uploadfileApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Bai17uploadfileApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 }
