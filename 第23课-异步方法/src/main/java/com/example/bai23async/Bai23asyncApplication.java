5d199d279d44c07d555055a04be3ede714ee2322
baiyuting <baiyuting@vipkid.com.cn>
2017-12-05 15:36:30 +0800

    第23课-异步方法

---
+++ 第23课-异步方法/src/main/java/com/example/bai23async/Bai23asyncApplication.java
@@ -0,0 +1,12 @@
+package com.example.bai23async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bai23asyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bai23asyncApplication.class, args);
+	}
+}
