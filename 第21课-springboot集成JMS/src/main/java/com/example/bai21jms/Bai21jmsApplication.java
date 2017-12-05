f890074d1771364a75227a5e7360a9ff73f10445
baiyuting <baiyuting@vipkid.com.cn>
2017-12-05 09:38:35 +0800

    第21课-springboot集成JMS

---
+++ 第21课-springboot集成JMS/src/main/java/com/example/bai21jms/Bai21jmsApplication.java
@@ -0,0 +1,12 @@
+package com.example.bai21jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bai21jmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bai21jmsApplication.class, args);
+	}
+}
---
+++ 第21课-springboot集成JMS/src/test/java/com/example/bai21jms/Bai21jmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.bai21jms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Bai21jmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
