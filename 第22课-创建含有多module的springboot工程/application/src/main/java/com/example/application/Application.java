903a5cf19c9d524e794811541a2a12db58d4ccc7
baiyuting <baiyuting@vipkid.com.cn>
2017-12-05 15:29:18 +0800

    第22课-创建含有多module的springboot工程 结束

--- 第22课-创建含有多module的springboot工程/application/src/main/java/com/example/application/Application.java
+++ 第22课-创建含有多module的springboot工程/application/src/main/java/com/example/application/Application.java
@@ -1,10 +1,31 @@
 package com.example.application;
 
+import com.example.library.Service;
+import com.example.library.ServiceConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@Import(ServiceConfiguration.class)
+@RestController
 public class Application {
+
+	private final Service service;
+
+	@Autowired
+	public Application(Service service) {
+		this.service = service;
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return service.message();
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
---
+++ 第22课-创建含有多module的springboot工程/library/src/main/java/com/example/library/Service.java
@@ -0,0 +1,21 @@
+package com.example.library;
+
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by fangzhipeng on 2017/4/19.
+ */
+@Component
+public class Service {
+
+    private final String message;
+
+    public Service(String message) {
+        this.message = message;
+    }
+
+    public String message() {
+        return this.message;
+    }
+}
