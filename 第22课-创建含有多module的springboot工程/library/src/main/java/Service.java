5d0120d642bbc8bd2eb02d709fc40c101622043b
baiyuting <baiyuting@vipkid.com.cn>
2017-12-05 10:50:18 +0800

    第22课-创建含有多module的springboot工程

---
+++ 第22课-创建含有多module的springboot工程/library/src/main/java/Service.java
@@ -0,0 +1,12 @@
+public class Service {
+
+    private final String message;
+
+    public Service(String message) {
+        this.message = message;
+    }
+
+    public String message() {
+        return this.message;
+    }
+}
